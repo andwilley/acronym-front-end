3b9010096d106a5a1f419f465d8563dfd0fbbfb9
Drew Willey <andwilley@gmail.com>
2019-04-28 13:47:26 -0700

    Just localhost as default for API.

--- src/const.js
+++ src/const.js
@@ -1,3 +1,3 @@
 export const ERROR = 'error';
 export const ERROR_API_RETURN = {acronyms: [{name: 'error', definition: 'request failed'}]};
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "localhost:8080";
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "localhost";
