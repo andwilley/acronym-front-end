6498152992d76f7b2b05ca589f756f342aafd213
Drew Willey <andwilley@gmail.com>
2021-12-12 20:59:26 -0700

    tset

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useReducer } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { type State } from './types/types';
+import { type Action } from './actions/actionCreators';
 import AcronymFinder from './components/AcronymFinder';
 import StoreContext from './components/StoreContext';
 import rootReducer from './reducers/rootReducer';
@@ -8,7 +10,7 @@ import { INITIAL_STATE } from './const';
 import './style/flexGrid.css';
 
 function App() {
-  const store = useReducer(rootReducer, INITIAL_STATE);
+  const store: [State, React.Dispatch<Action>] = useReducer(rootReducer, INITIAL_STATE);
 
   return (
     <StoreContext.Provider value={store}>
